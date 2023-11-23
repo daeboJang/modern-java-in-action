8a3ddf60b5a6c669c398820761d15612ea6f53ac
JangKwangHee <nogovern@gmail.com>
2023-11-23 14:06:30 +0900

    2.9 JUnit5로 예제 테스트 추가

--- src/test/java/book/RealWorldSoftwareDev/ch_02/BankStatementCSVParserTest.java
+++ src/test/java/book/RealWorldSoftwareDev/ch_02/BankStatementCSVParserTest.java
@@ -1,5 +1,6 @@
 package book.RealWorldSoftwareDev.ch_02;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
@@ -10,17 +11,20 @@ class BankStatementCSVParserTest {
     private  final BankStatementParser statementParser = new BankStatementCSVParser();
 
     @Test
-    public void test1() {
-//        Assertions.fail("not yet implemented!!");
+    @DisplayName("1줄 파싱")
+    public void shouldParseOneCorrectLine() {
+        //given - when - then
         final String line = "30-01-2017,-50,Tesco";
 
         final BankTransaction result = statementParser.parseFrom(line);
 
+        // 예상 결과 BankTransaction 인스턴스 생성
         final BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
         final double tolerance = 0.0d;
 
-        assertThat(expected.getDate()).isEqualTo(result.getDate());
-        assertThat(result.getAmount()).isEqualTo(50);
+        assertThat(result.getDate()).isEqualTo(expected.getDate());
+        assertThat(result.getAmount()).isEqualTo(expected.getAmount());
+        assertThat(result.getDate()).isEqualTo(expected.getDate());
     }
 
 }
