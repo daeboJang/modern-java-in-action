21ed6df27e8371cd4c067d50b24c7332553c0a22
JangKwangHee <nogovern@gmail.com>
2023-12-07 14:57:37 +0900

    테스트 코드 보완

--- src/main/java/book/real_world_software/ch05/BusinessRuleEngine.java
+++ src/main/java/book/real_world_software/ch05/BusinessRuleEngine.java
@@ -1,13 +1,22 @@
 package book.real_world_software.ch05;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BusinessRuleEngine {
 
+    private final List<Action> actions;
+
+    public BusinessRuleEngine() {
+        this.actions = new ArrayList<>();
+    }
+
     public void addAction(final Action action) {
-        throw new UnsupportedOperationException();
+        this.actions.add(action);
     }
 
     public int count() {
-        throw new UnsupportedOperationException();
+        return this.actions.size();
     }
 
     public void run() {
